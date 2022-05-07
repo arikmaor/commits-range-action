87f3cc57c561eb8c80e79db41295db94c68d5e96
Arik Maor <arikmaor@gmail.com>
2022-05-07 04:29:54 +0300

    Refactor test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,13 +4,16 @@ import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
 test('test runs', () => {
-  process.env['GITHUB_REPOSITORY'] = 'arikmaor/commits-range-action'
-  process.env['INPUT_BASE_REVISION'] = 'HEAD^^^'
-  process.env['INPUT_HEAD_REVISION'] = 'HEAD'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
-    env: process.env
+    env: {
+      ...process.env,
+      GITHUB_REPOSITORY:
+        process.env['GITHUB_REPOSITORY'] || 'arikmaor/commits-range-action',
+      INPUT_BASE_REVISION: process.env['INPUT_BASE_REVISION'] || 'HEAD^^^',
+      INPUT_HEAD_REVISION: process.env['INPUT_HEAD_REVISION'] || 'HEAD'
+    }
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
